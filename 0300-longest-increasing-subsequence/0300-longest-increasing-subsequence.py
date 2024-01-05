8845960dedbc6610c2fa396e74f5778fff1156e6
vikramlance <vikram.lance@gmail.com>
2024-01-05 14:53:31 -0600

    Time: 1040 ms (73.59%), Space: 17.8 MB (14.87%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,12 +1,9 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         
-        # dp[i] is lis for list until ith element 
-        # for i = 0 - base case - dp[0] = 1 
-        # i = 2 if nums[i] < nums[j]
-        
-        
-        # use j for iterating dp , use i for iterating nums 
+        # dp[i] is lis for list until ith element for example nums = [4,56,100] then dp = [1,2,3]
+        # set all the dp elements to 1 cause minimum lis is 1 ( that is the elements itself if no other element is dound that is greater than the current element)
+        # iterate over nums and for each sub array dp 0 to i -1 , we check if nums[i] > nums[j] if it is greater then we update the dp to max dp[i], dp[j] + 1 
         
         
         dp = [1] * len(nums)

93ee3dc5a9998288bffe4c58e1f3c81f1d931293
vikramlance <vikram.lance@gmail.com>
2024-01-14 08:22:58 -0600

    Time: 111 ms (61.24%), Space: 17.8 MB (28.75%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,28 +1,24 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         # ebcbbececab bacecbbcbe
-        
-        def is_palin(s):
-            l,r = 0, len(s) -1
-            while l< r:
-                if s[l] != s[r]:
-                    return False
-                else:
-                    r -= 1
-                    l += 1
-            return True
-            
+
         l,r = 0, len(s) -1
         while l< r:
             if s[l] != s[r]:
-                return is_palin(s[l:r]) or is_palin(s[l+1:r+1])
+                return self.is_palin(s[l:r]) or self.is_palin(s[l+1:r+1])
             else:
                 r -= 1
                 l += 1
         return True
-        
-        
-        
-                
+
+    def is_palin(self, s):
+        l,r = 0, len(s) -1
+        while l< r:
+            if s[l] != s[r]:
+                return False
+            else:
+                r -= 1
+                l += 1
+        return True         
         
         

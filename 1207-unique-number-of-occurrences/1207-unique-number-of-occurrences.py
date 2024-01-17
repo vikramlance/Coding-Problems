0a3d21d08bdb9bdb694361ece513e1423f4e43d3
vikramlance <vikram.lance@gmail.com>
2024-01-16 19:36:42 -0600

    Time: 31 ms (98.44%), Space: 17.2 MB (47.75%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,18 +1,24 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        # value of i is from -1000 to 1000 so we can use co
+        # value of i is from -1000 to 1000 so we can use counting sort
+        # first frequency of the nums and then count the frequency 
         
-        freq = {}
+        nums = [0]*2001
+        count = [0]*1001
         
         for i in arr:
-            freq[i] = freq.get(i, 0) + 1
+            nums[i+1000] += 1
         
-        count_set = set()
-        
-        for count in freq.values():
-            if count not in count_set:
-                count_set.add(count)
-            else:
+        for k in range(2001):
+            if nums[k] and count[nums[k]] + 1 > 1:
                 return False
+            count[nums[k]] += 1
+        
         return True
         
+        """
+        1001 1 
+        1002 1
+        
+        0-1000
+        """
